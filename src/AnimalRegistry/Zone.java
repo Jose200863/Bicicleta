85cf4d9960116189a0a0b1072256a15e09fecb71
Brayan <brazofeifaam@est.utn.ac.cr>
2025-06-23 07:12:59 -0600

    Clase enum zona
    Agregado enum Zona con las zonas disponibles del zoológico

---
+++ src/AnimalRegistry/Zone.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package AnimalRegistry;
+
+/**
+ *
+ * @author Braya
+ */
+public enum Zone {
+    FARM("Farm"),
+    CONTACT("Contact"),
+    FEEDING("Feeding"),
+    CONSERVATIO_AND_RESCUE("Conservation and rescue"),
+    NIGHT_TUNEL("Night tunel"),
+    LOCAL_BIODIVERSITY("Local biodiversity");
+    
+    private final String Zone;
+
+    Zone(String Zone) {
+        this.Zone = Zone;
+    }
+
+    public String getZone() {
+        return Zone;
+    }
+
+    @Override
+    public String toString() {
+        return "Zone " + "Zone: " + Zone;
+    } 
+}
